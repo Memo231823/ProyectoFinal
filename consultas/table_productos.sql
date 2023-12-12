955dea98525ac77c87037b1b181a6a1bcd169368
Memo Santos <78324835+Memo231823@users.noreply.github.com>
2023-12-12 02:01:00 -0600

    Actualizar table_productos.sql

--- consultas/table_productos.sql
+++ consultas/table_productos.sql
@@ -1,9 +1,9 @@
 CREATE TABLE productos (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    descripcion TEXT,
-    precio DECIMAL(10, 2) NOT NULL,
+    codigo VARCHAR(100) NOT NULL,
+    descripcion TEXT
+    precio_venta DECIMAL(10, 2) NOT NULL,
     cantidad INT NOT NULL,
     img TEXT,
-    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
