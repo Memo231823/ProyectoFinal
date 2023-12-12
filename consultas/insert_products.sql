ede28d4038a7c9dfa9a991c91e9566c9e0410905
Memo Santos <78324835+Memo231823@users.noreply.github.com>
2023-12-12 01:58:10 -0600

    Actualizar insert_products.sql

--- consultas/insert_products.sql
+++ consultas/insert_products.sql
@@ -1,7 +1,1 @@
-INSERT INTO productos (nombre, descripcion, precio, cantidad)
-VALUES
-    ('Producto 1', 'Descripción del Producto 1', 25.99, 50),
-    ('Producto 2', 'Descripción del Producto 2', 19.99, 100),
-    ('Producto 3', 'Descripción del Producto 3', 34.50, 75),
-    ('Producto 4', 'Descripción del Producto 4', 45.75, 30),
-    ('Producto 5', 'Descripción del Producto 5', 55.00, 60);
+INSERT INTO productos (codigo, descripcion, precio_compra, precio_venta, cantidad, img,fecha, ventas, id_medida, id_categoria)
